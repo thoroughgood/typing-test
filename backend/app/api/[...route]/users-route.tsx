708facd733deb4fbd41474f1e56c044eb090c44b
tez <thoroughgoodterry@gmail.com>
2026-04-13 19:31:55 +1000

    removed password from schema, OAuth handles all authentication

--- backend/app/api/[...route]/users-route.tsx
+++ backend/app/api/[...route]/users-route.tsx
@@ -1,12 +1,19 @@
 import { Hono } from 'hono';
 import db from '@/src/db/';
+import userInsert from '@/src/db/schema';
 import { usersTable } from '@/src/db/schema';
 
 const usersRoute = new Hono();
+type newUser = typeof userInsert;
 
 usersRoute.get('/', async (c) => {
   const users = await db.select().from(usersTable);
   return c.json(users);
 });
 
+usersRoute.post('/add', async (c) => {
+  //post route waits for information to be sent
+  const { id, username, email } = await c.req.json();
+  await db.insert(usersTable).values({ username, email });
+});
 export default usersRoute;
--- backend/src/db/schema.tsx
+++ backend/src/db/schema.tsx
@@ -4,7 +4,6 @@ export const usersTable = sqliteTable('users_table', {
   id: int().primaryKey({ autoIncrement: true }),
   username: text().notNull(),
   email: text().notNull().unique(),
-  password_hash: text().notNull(),
   created_at: text().default(sql`(CURRENT_DATE)`),
 });
 
