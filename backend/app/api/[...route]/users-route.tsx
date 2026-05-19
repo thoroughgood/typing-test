f3b398ef5acf8bb18cdf0318481d8a54904631bc
tez <thoroughgoodterry@gmail.com>
2026-05-19 23:11:17 +1000

    missed in previous .add

--- backend/app/api/[...route]/users-route.tsx
+++ backend/app/api/[...route]/users-route.tsx
@@ -1,19 +1,19 @@
 import { Hono } from 'hono';
 import db from '@/src/db/';
 import { eq } from 'drizzle-orm';
-import { usersTable } from '@/src/db/schema';
+import { Users } from '@/src/db/schema';
 
 const usersRoute = new Hono();
 
 usersRoute.get('/', async (c) => {
-  const users = await db.select().from(usersTable);
+  const users = await db.select().from(Users);
   return c.json(users);
 });
 
 usersRoute.post('/add', async (c) => {
   //post route waits for information to be sent
-  const { id, username, email } = await c.req.json();
-  await db.insert(usersTable).values({ id, username, email });
+  const { auth0_id, username, email } = await c.req.json();
+  await db.insert(Users).values({ auth0_id, username, email });
   return c.text('add users');
 });
 
@@ -21,8 +21,8 @@ usersRoute.get('/:id', async (c) => {
   const { id } = await c.req.json();
   const user = await db
     .select()
-    .from(usersTable)
-    .where(eq(usersTable.id, id))
+    .from(Users)
+    .where(eq(Users.id, id))
     .get();
   if (!user) {
     return c.json({ error: 'User does not exist' }, 404);
--- backend/src/db/schema.tsx
+++ backend/src/db/schema.tsx
@@ -8,7 +8,7 @@ import { real } from 'drizzle-orm/sqlite-core';
 //username: display name for user, text
 //email: email address for things like forgot password etc, text
 //created_at: creation date, text
-export const usersTable = sqliteTable('users_table', {
+export const Users = sqliteTable('users_table', {
   id: int().primaryKey({ autoIncrement: true }), //Native id
   auth0_id: text().notNull().unique(), //Unique identifier
   username: text().notNull(),
@@ -23,7 +23,7 @@ export const typingTests = sqliteTable('typing_test', {
   id: int().notNull().primaryKey({ autoIncrement: true }),
   userId: int()
     .notNull()
-    .references(() => usersTable.id),
+    .references(() => Users.id),
   wpm: int().notNull(),
   acc: real().notNull(),
   createdAt: text().default(sql`(CURRENT_DATE)`),
@@ -32,16 +32,47 @@ export const typingTests = sqliteTable('typing_test', {
 //userStats table
 //requires user id (one to one relationship)
 //userId: int -> foreign key so references
-export const userStats = sqliteTable('stats_table' {
-  userId: int().notNull().references(() => usersTable.id),
+export const userStats = sqliteTable('stats_table', {
+  userId: int()
+    .notNull()
+    .references(() => Users.id),
   acc: real(),
   avgWpm: real(),
   topWpm: int(),
   testsCompleted: int(),
-})
+});
+
+//relations syntax -> (table, ({}))
+
+export const typingTestsRelations = relations(
+  typingTests,
+  ({ one }) => ({
+    //one -> shows one to one connection, and the field of the typing test is the typingTest userID, which references the users ID
+    user: one(Users, {
+      fields: [typingTests.userId],
+      references: [Users.id],
+    }),
+  }),
+);
+
+export const userStatsRelations = relations(userStats, ({ one }) => ({
+  user: one(Users, {
+    fields: [userStats.userId],
+    references: [Users.id],
+  }),
+}));
+
+export const userRelations = relations(Users, ({ many, one }) => ({
+  typingTests: many(typingTests),
+  stats: one(userStats),
+}));
+
+//Why we do these relations:
+//we can now do const user = await db.query.users.findFirst({with: {typingTests:true/false, stats:true/false}})
+//this means we can pull multiple tables with one query because we have defined their relationships
 
 //insertion into database
 //await db.insert(typingTests).values({user_id:xyz, wpm:xyz, acc:xyz, createdAt:xyz}) -> as we insert into databse the autoincrement for typing tests will generate unique ids
 
-export type User = typeof usersTable.$inferSelect;
-export type InsertUser = typeof usersTable.$inferInsert;
+export type User = typeof Users.$inferSelect;
+export type InsertUser = typeof Users.$inferInsert;
