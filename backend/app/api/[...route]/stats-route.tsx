fa5f109caa3e0e89be4cf1ea4b24d88376e7b25c
tez <thoroughgoodterry@gmail.com>
2026-08-10 00:01:37 +1000

    haven't tested, theoretical stats route

--- backend/app/api/[...route]/stats-route.tsx
+++ backend/app/api/[...route]/stats-route.tsx
@@ -1,7 +1,7 @@
 import { Hono } from 'hono';
 import db from '@/src/db/';
 import { count, eq } from 'drizzle-orm';
-import { userStats, typingTests } from '@/src/db/schema';
+import { userStats } from '@/src/db/schema';
 import { get } from 'https';
 
 const statsRoute = new Hono();
@@ -12,13 +12,47 @@ interface statsData {
 
 statsRoute.post('/update', async (c) => {
   //get results including wpm and acc
-  const userId = await c.req.json();
+  const { userId, acc, wpm } = await c.req.json();
   //grab number of typing tests for userId
-  const originalCount = await db
-    .select({ count: count() })
-    .from(typingTests)
-    .where(eq(typingTests.userId, userId));
-  const newCount = originalCount[0].count + 1;
+  const userInfo = await db
+    .select()
+    .from(userStats)
+    .where(eq(userStats.userId, userId));
+  const testsCompleted = userInfo[0].testsCompleted
+    ? userInfo[0].testsCompleted
+    : 0;
+  const newTestsTaken = userInfo[0].testsCompleted
+    ? userInfo[0].testsCompleted + 1
+    : 1;
+
+  const newAcc = userInfo[0].acc
+    ? (userInfo[0].acc * testsCompleted + acc) / newTestsTaken
+    : 0;
+  const newAvgWpm = userInfo[0].avgWpm
+    ? (userInfo[0].avgWpm * testsCompleted + wpm) / newTestsTaken
+    : 0;
+  const newTopWpm = userInfo[0].topWpm
+    ? Math.max(userInfo[0].topWpm, wpm)
+    : wpm;
+
+  const updatedUserInfo = await db
+    .insert(userStats)
+    .values({
+      userId,
+      acc: newAcc,
+      avgWpm: newAvgWpm,
+      topWpm: newTopWpm,
+    })
+    .onConflictDoUpdate({
+      target: userId,
+      set: {
+        userId: userId,
+        acc: newAcc,
+        avgWpm: newAvgWpm,
+        topWpm: newTopWpm,
+      },
+    });
+
   //  get new count -> new avg is (old avg * old count + new wpm) / new count
   // same for avg wpm
   // top wpm is max current test wpm vs old top wpm this needs to update on a new test entry -> send from api to api
