557703faf304851453a134a84abf2a8e37127ddf
tez <thoroughgoodterry@gmail.com>
2026-08-24 19:23:23 +1000

    small changes to fix warnings

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,5 +1,5 @@
-import type { NextRequest } from 'next/server';
-import { auth0 } from './lib/auth0';
+import { NextRequest } from 'next/server';
+import { auth0 } from '@/lib/auth0';
 
 export async function middleware(request: NextRequest) {
   return await auth0.middleware(request);
@@ -7,12 +7,8 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-     */
     '/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
   ],
 };
+
+export const runtime = 'nodejs';
