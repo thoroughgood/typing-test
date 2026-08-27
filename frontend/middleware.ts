c536b6d6cd95aaa8fe26cc8ec5e47e57b90d5496
tez <thoroughgoodterry@gmail.com>
2026-08-27 14:57:40 +1000

    god knows

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,5 +1,12 @@
 import { NextRequest } from 'next/server';
-import { auth0 } from './lib/auth0';
+import { Auth0Client } from '@auth0/nextjs-auth0/server';
+
+const auth0 = new Auth0Client({
+  authorizationParameters: {
+    scope: process.env.AUTH0_SCOPE,
+    audience: process.env.AUTH0_AUDIENCE,
+  },
+});
 
 export async function middleware(request: NextRequest) {
   return await auth0.middleware(request);
