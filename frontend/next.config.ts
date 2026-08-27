9009e0c1d40c4218c814cd1474de51da51467b93
tez <thoroughgoodterry@gmail.com>
2026-08-27 16:44:56 +1000

    more vercel fixes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,17 +1,17 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    // @ts-expect-error Node middleware is not yet included in Next 15.5's config types.
-    nodeMiddleware: true,
-  },
-};
-
-module.exports = {
   images: {
     remotePatterns: [
-      new URL('https://lh3.googleusercontent.com/a/**'),
-      new URL('https://s.gravatar.com'),
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/a/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 's.gravatar.com',
+      },
     ],
   },
 };
