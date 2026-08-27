cb40158eb810ad299a3a90fd0c1f696c49888920
tez <thoroughgoodterry@gmail.com>
2026-08-27 14:54:20 +1000

    fixed local @lib alias issue

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,5 +1,5 @@
 import { NextRequest } from 'next/server';
-import { auth0 } from '@/lib/auth0';
+import { auth0 } from './lib/auth0';
 
 export async function middleware(request: NextRequest) {
   return await auth0.middleware(request);
